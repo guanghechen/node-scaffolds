6d1be9c54cb44c5631207ae71d4ed6746c168b6c
guanghechen <me.guanghechen@gmail.com>
2021-03-05 23:27:27 +0800

    :sparkles:  feat: add new sub-package @guanghechen/rollup-config-cli

---
+++ packages/rollup-config-cli/src/index.ts
@@ -0,0 +1,65 @@
+import type { RollupConfigOptions as BaseRollupConfigOptions } from '@guanghechen/rollup-config'
+import createBaseRollupConfig from '@guanghechen/rollup-config'
+import type { RollupPluginCopyOptions } from '@guanghechen/rollup-plugin-copy'
+import copy from '@guanghechen/rollup-plugin-copy'
+import type { RollupOptions } from 'rollup'
+
+export interface RollupConfigOptions extends BaseRollupConfigOptions {
+  /**
+   * Options of @guanghechen/rollup-plugin-copy, for coping resources or config files.
+   */
+  resources?: RollupPluginCopyOptions
+  /**
+   * Node.js bin targets.
+   */
+  targets: Array<{
+    /**
+     * Source file path, such as 'src/cli.ts'.
+     */
+    src: string
+    /**
+     * Target file path, such as 'lib/cjs/cli.js'.
+     * The target filepath should be placed in the same directory as the entry file.
+     */
+    target: string
+  }>
+}
+
+export function createRollupConfig(
+  options: RollupConfigOptions,
+): RollupOptions[] {
+  const { resources, targets, ...baseOptions } = options
+  const baseConfig = createBaseRollupConfig(baseOptions)
+
+  const { plugins = [] } = baseConfig
+  const external = baseConfig as (id: string) => boolean
+
+  const configs: RollupOptions[] = [
+    {
+      ...baseConfig,
+      plugins: [...plugins, copy(resources)],
+    },
+    ...targets.map(
+      (item): RollupOptions => ({
+        input: item.src,
+        output: [
+          {
+            file: item.target,
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true,
+            banner: '#! /usr/bin/env node',
+          },
+        ],
+        external: (id: string): boolean => {
+          if (external(id)) return true
+          return /\.\/index$/.test(id) || /^[.]+$/.test(id)
+        },
+        plugins,
+      }),
+    ),
+  ]
+  return configs
+}
+
+export default createRollupConfig
