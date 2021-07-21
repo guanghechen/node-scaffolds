5887f04f7204aa549fefecf40d2ec1634f476bff
guanghechen <me.guanghechen@gmail.com>
2021-07-21 14:24:04 +0800

    :construction:  improve(blob-helper): perform clear actions after downloaded

--- packages/blob-helper/src/download.ts
+++ packages/blob-helper/src/download.ts
@@ -1,17 +1,31 @@
 /**
- * emit a download task in browser
+ * Emit a download task in browser.
+ *
  * @param blob
  * @param filename
  */
 export function downloadBlob(blob: Blob, filename: string): void {
-  const url = window.URL.createObjectURL(blob)
+  // For ie browser
+  if (window.navigator.msSaveBlob != null) {
+    window.navigator.msSaveBlob(blob, filename)
+    return
+  }
 
-  const a: HTMLAnchorElement = document.createElement('a')
-  a.style.display = 'none'
-  a.href = url
-  a.download = filename
+  // For other browsers
+  const anchor: HTMLAnchorElement = document.createElement('a')
+  let url: string | null = null
+  try {
+    url = window.URL.createObjectURL(blob)
+    anchor.style.display = 'none'
+    anchor.href = url
+    anchor.setAttribute('download', filename)
 
-  document.body.appendChild(a)
-  a.click()
-  window.URL.revokeObjectURL(url)
+    // Starting download.
+    document.body.appendChild(anchor)
+    anchor.click()
+  } finally {
+    // Perform clean actions.
+    document.body.removeChild(anchor)
+    if (url != null) window.URL.revokeObjectURL(url)
+  }
 }
