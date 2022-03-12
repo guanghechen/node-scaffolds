b0a349138652d5234a5b3c0f5abb5a0412997908
guanghechen <me.guanghechen@gmail.com>
2022-03-12 14:29:14 +0800

    :bug:  fix: useReactiveRef should always pointer to the latest referred value

--- packages/react-hooks/src/useReactiveRef.ts
+++ packages/react-hooks/src/useReactiveRef.ts
@@ -1,15 +1,12 @@
-import type React from 'react'
-import { useEffect, useRef } from 'react'
+import React from 'react'
 
 /**
  * Create a reactive ref which will follow the changes of the given data.
  * @param data
  * @returns
  */
-export function useReactiveRef<T>(data: T): React.MutableRefObject<T> {
-  const ref = useRef(data)
-  useEffect(() => {
-    ref.current = data
-  }, [data])
+export function useReactiveRef<T>(value: T): React.MutableRefObject<T> {
+  const ref = React.useRef<T>(value)
+  ref.current = value
   return ref
 }
