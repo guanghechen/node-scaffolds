4d6704a47106d3d2b070bfbaaf83ecefbd5e5877
guanghechen <me.guanghechen@gmail.com>
2022-08-27 19:12:04 +0800

    :wrench:  chore: fix build error

---
+++ packages/rollup-config-tsx/rollup.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import createRollupConfig from '@guanghechen/rollup-config'
+import path from 'path'
+
+async function rollupConfig() {
+  const { default: manifest } = await import(path.resolve('package.json'))
+  manifest.types = undefined
+
+  const config = createRollupConfig({
+    manifest,
+    pluginOptions: {
+      typescriptOptions: { tsconfig: 'tsconfig.src.json' },
+    },
+  })
+  return config
+}
+
+export default rollupConfig()
