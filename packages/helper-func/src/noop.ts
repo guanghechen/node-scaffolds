67584dc886d759aad51232472fe141bf35d9c3c9
guanghechen <me.guanghechen@gmail.com>
2022-09-16 22:04:40 +0800

    :rotating_light:  fix lint warnings

--- packages/helper-func/src/noop.ts
+++ packages/helper-func/src/noop.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const noop = (...args: any[]): void => {}
+export const noop = (..._args: any[]): void => {}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const noopAsync = (...args: any[]): Promise<void> => Promise.resolve()
+export const noopAsync = (..._args: any[]): Promise<void> => Promise.resolve()
