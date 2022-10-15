e77633cd892c2e25635cc2f8bf4b6d0635f729ff
guanghechen <me.guanghechen@gmail.com>
2022-10-15 12:44:40 +0800

    :green_heart:  chore: fix test

--- packages/helper-commander/__test__/util-ansi.spec.ts
+++ packages/helper-commander/__test__/util-ansi.spec.ts
@@ -1,11 +1,7 @@
-import chalk from 'chalk'
 import { stripAnsi } from '../src'
 
 describe('stripAnsi', () => {
   test('basic', () => {
-    const text = 'Hello, world!'
-    const coloredText = chalk.gray(text)
-    expect(coloredText).not.toEqual(text)
-    expect(stripAnsi(coloredText)).toEqual(text)
+    expect(stripAnsi('[32mHello, world![39m')).toEqual('Hello, world!')
   })
 })
