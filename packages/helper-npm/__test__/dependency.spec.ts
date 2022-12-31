645060faf63a5eadc8255272aba52e8eb411ba39
guanghechen <me.guanghechen@gmail.com>
2022-12-31 19:02:28 +0800

    :sparkles:  [BREAKING] improve(helper-npm) support ESM format only

--- packages/helper-npm/__test__/dependency.spec.ts
+++ packages/helper-npm/__test__/dependency.spec.ts
@@ -2,9 +2,11 @@ import { jest } from '@jest/globals'
 import { desensitize, locateFixtures } from 'jest.helper'
 import path from 'node:path'
 import url from 'node:url'
-import { collectAllDependencies, getDefaultDependencyFields, locateLatestPackageJson } from '../src'
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+import {
+  collectAllDependencies,
+  getDefaultDependencyFields,
+  locateLatestPackageJson,
+} from '../src/dependency'
 
 describe('getDefaultDependencyFields', () => {
   test('basic', () => {
@@ -17,96 +19,45 @@ describe('getDefaultDependencyFields', () => {
 })
 
 describe('collectAllDependencies', () => {
-  test('basic', () => {
-    expect(collectAllDependencies(path.join(__dirname, '../package.json'))).toEqual([
-      '@guanghechen/helper-path',
-      '@guanghechen/invariant',
-    ])
+  test('current repo', async () => {
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+    const dependencies = await collectAllDependencies(path.join(__dirname, '../package.json'))
 
-    expect(
-      collectAllDependencies(
-        locateFixtures('monorepo2/package.json'),
-        undefined,
-        ['rollup'],
-        () => true,
-      ),
-    ).toEqual([
-      '@ampproject/remapping',
-      '@babel/code-frame',
-      '@babel/compat-data',
-      '@babel/core',
-      '@babel/generator',
-      '@babel/helper-compilation-targets',
-      '@babel/helper-environment-visitor',
-      '@babel/helper-function-name',
-      '@babel/helper-hoist-variables',
-      '@babel/helper-module-imports',
-      '@babel/helper-module-transforms',
-      '@babel/helper-simple-access',
-      '@babel/helper-split-export-declaration',
-      '@babel/helper-string-parser',
-      '@babel/helper-validator-identifier',
-      '@babel/helper-validator-option',
-      '@babel/helpers',
-      '@babel/highlight',
-      '@babel/parser',
-      '@babel/template',
-      '@babel/traverse',
-      '@babel/types',
-      '@jridgewell/gen-mapping',
-      '@jridgewell/resolve-uri',
-      '@jridgewell/set-array',
-      '@jridgewell/sourcemap-codec',
-      '@jridgewell/trace-mapping',
-      'ansi-styles',
-      'browserslist',
-      'caniuse-lite',
-      'chalk',
-      'color-convert',
-      'color-name',
-      'convert-source-map',
-      'debug',
-      'electron-to-chromium',
-      'escalade',
-      'fsevents',
-      'gensync',
-      'globals',
-      'has-flag',
-      'js-tokens',
-      'jsesc',
-      'json5',
-      'lru-cache',
-      'ms',
-      'node-releases',
-      'picocolors',
-      'rollup',
-      'semver',
-      'supports-color',
-      'to-fast-properties',
-      'type-fest',
-      'update-browserslist-db',
-      'yallist',
-    ])
+    expect(dependencies).toEqual(['@guanghechen/helper-path', 'import-meta-resolve'])
+  })
 
+  test('nonexistent repo', async () => {
+    const dependencies = await collectAllDependencies(
+      locateFixtures('nonexistent/package.json'),
+      undefined,
+      ['rollup'],
+      () => true,
+    )
+
+    expect(dependencies).toEqual(['@guanghechen/helper-path', 'import-meta-resolve', 'rollup'])
+  })
+
+  test('normal repo', async () => {
     const warningDataList: unknown[] = []
     const warnSpy = jest
       .spyOn(console, 'warn')
       .mockImplementation((...args) => void warningDataList.push(...args))
 
-    expect(
-      collectAllDependencies(locateFixtures('normal-repo/package.json'), ['peerDependencies']),
-    ).toEqual(['@guanghechen/not-existed-repo', 'rollup'])
+    const dependencies = await collectAllDependencies(locateFixtures('normal-repo/package.json'), [
+      'peerDependencies',
+    ])
+    expect(dependencies).toEqual(['@guanghechen/not-existed-repo', 'rollup'])
 
     expect(desensitize(warningDataList)).toEqual([
       "cannot find package.json for '@guanghechen/not-existed-repo'",
     ])
-
     warnSpy.mockRestore()
   })
 })
 
 describe('locateLatestPackageJson', () => {
   test('basic', () => {
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
     expect(locateLatestPackageJson(__dirname)).toBe(path.join(__dirname, '../package.json'))
   })
 })
--- packages/helper-npm/src/dependency.ts
+++ packages/helper-npm/src/dependency.ts
@@ -1,4 +1,5 @@
 import { locateNearestFilepath } from '@guanghechen/helper-path'
+import { resolve } from 'import-meta-resolve'
 import fs from 'node:fs'
 
 export type IDependencyField = 'dependencies' | 'optionalDependencies' | 'peerDependencies'
@@ -21,31 +22,34 @@ export const getDefaultDependencyFields = (): IDependencyField[] => [
  * @param isAbsentAllowed
  * @returns
  */
-export function collectAllDependencies(
+export async function collectAllDependencies(
   packageJsonPath: string | null,
   dependenciesFields: ReadonlyArray<IDependencyField> = getDefaultDependencyFields(),
   additionalDependencies: ReadonlyArray<string> | null = null,
   isAbsentAllowed: ((moduleName: string) => boolean) | null = null,
-): string[] {
-  const result: string[] = []
+): Promise<string[]> {
+  const dependencySet: Set<string> = new Set()
+
   if (isAbsentAllowed == null) {
     const regex = /^@types\//
     // eslint-disable-next-line no-param-reassign
     isAbsentAllowed = moduleName => regex.test(moduleName)
   }
 
-  const followDependency = (dependency: string): void => {
-    if (result.includes(dependency)) return
-    result.push(dependency)
+  const followDependency = async (dependency: string): Promise<void> => {
+    if (dependencySet.has(dependency)) return
+    dependencySet.add(dependency)
 
     // recursively collect
     let nextPackageJsonPath = null
     try {
-      const dependencyPath = require.resolve(dependency)
-      nextPackageJsonPath = locateLatestPackageJson(dependencyPath)
+      const dependencyPath = await resolve(dependency, import.meta.url)
+      if (dependencyPath != null) {
+        nextPackageJsonPath = locateNearestFilepath(dependencyPath, 'package.json')
+      }
     } catch (e: any) {
       switch (e.code) {
-        case 'MODULE_NOT_FOUND':
+        case 'ERR_MODULE_NOT_FOUND':
           if (isAbsentAllowed!(dependency)) return
           break
         case 'ERR_PACKAGE_PATH_NOT_EXPORTED':
@@ -61,14 +65,14 @@ export function collectAllDependencies(
       return
     }
 
-    collectDependencies(nextPackageJsonPath)
+    await collectDependencies(nextPackageJsonPath)
   }
 
   /**
    * @param {string} dependencyPackageJsonPath
    * @returns {void}
    */
-  const collectDependencies = (dependencyPackageJsonPath: string): void => {
+  const collectDependencies = async (dependencyPackageJsonPath: string): Promise<void> => {
     if (!fs.existsSync(dependencyPackageJsonPath)) {
       console.warn(`no such file or directory: ${dependencyPackageJsonPath}`)
       return
@@ -80,7 +84,7 @@ export function collectAllDependencies(
       const field = manifest[fieldName]
       if (field != null) {
         for (const dependency of Object.keys(field)) {
-          followDependency(dependency)
+          await followDependency(dependency)
         }
       }
     }
@@ -88,17 +92,17 @@ export function collectAllDependencies(
 
   // collect from package.json
   if (packageJsonPath != null) {
-    collectDependencies(packageJsonPath)
+    await collectDependencies(packageJsonPath)
   }
 
   // collect from dependencies
   if (additionalDependencies != null) {
     for (const dependency of additionalDependencies) {
-      followDependency(dependency)
+      await followDependency(dependency)
     }
   }
 
-  return result.sort()
+  return Array.from(dependencySet).sort()
 }
 
 /**
