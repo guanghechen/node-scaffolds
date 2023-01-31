4492c249cab76f26ad0a1565c37d7e50860c4e09
guanghechen <me.guanghechen@gmail.com>
2023-01-31 19:41:16 +0800

    :art:  improve(helper-func): prefer Iterable instead of ReadonlyArray

--- packages/helper-func/src/list2map.ts
+++ packages/helper-func/src/list2map.ts
@@ -1,13 +1,13 @@
 export const list2map = <K, T>(
-  elements: ReadonlyArray<T>,
+  elements: Iterable<T>,
   getKey: (element: T, index: number) => K,
 ): Map<K, T> => {
   const map: Map<K, T> = new Map()
-  const N = elements.length
-  for (let i = 0; i < N; ++i) {
-    const value = elements[i]
+  let i = 0
+  for (const value of elements) {
     const key = getKey(value, i)
     map.set(key, value)
+    i += 1
   }
   return map
 }
