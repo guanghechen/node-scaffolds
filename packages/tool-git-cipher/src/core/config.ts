3e4df328f9973c920e2da460194c1bc6c530ef3b
guanghechen <me.guanghechen@gmail.com>
2023-05-02 00:23:30 +0800

    :fire:  improve: remove unused codes

--- packages/tool-git-cipher/src/core/config.ts
+++ packages/tool-git-cipher/src/core/config.ts
@@ -1,25 +1,11 @@
 import path from 'node:path'
 import url from 'node:url'
 
-// Config files root dir
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const configRootDir = path.resolve(__dirname, '../config')
-
 // Template files root dir
-const boilerplateRootDir = path.join(configRootDir, 'boilerplates')
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const boilerplateRootDir = path.join(__dirname, '../config/boilerplates')
 
-/**
- * Calc absolute path of configs
- * @param filepath
- */
-export function resolveConfigFilepath(...filepath: string[]): string {
-  return path.resolve(configRootDir, ...filepath)
-}
-
-/**
- * Calc absolute path of template files
- * @param filepath
- */
+// Calc absolute path of template files
 export function resolveTemplateFilepath(...filepath: string[]): string {
   return path.resolve(boilerplateRootDir, ...filepath)
 }
