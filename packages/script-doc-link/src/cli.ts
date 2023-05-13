5490d2d081f4802baf2f0cce19c5c51bab7ab666
guanghechen <me.guanghechen@gmail.com>
2023-05-13 11:19:06 +0800

    :art:  improve(script-doc-link): add cli script

---
+++ packages/script-doc-link/src/cli.ts
@@ -0,0 +1,40 @@
+import { ChalkLogger, Level } from '@guanghechen/chalk-logger'
+import minimist from 'minimist'
+import path from 'node:path'
+import { resolveMonorepoDocLinkRewrite } from '.'
+
+async function run(argv: string[]): Promise<void> {
+  const logger = new ChalkLogger(
+    {
+      name: 'doc-link-rewrite',
+      level: Level.INFO,
+      flights: {
+        date: false,
+        colorful: true,
+      },
+    },
+    argv,
+  )
+
+  const args = minimist(argv)
+  const repoType = (args.repoType ?? 'monorepo').toLowerCase()
+  logger.debug('repoType:', repoType)
+
+  switch (repoType) {
+    case 'monorepo': {
+      const rootDir: string = args.rootDir ?? path.resolve()
+      logger.debug('rootDir:', rootDir)
+      await resolveMonorepoDocLinkRewrite({
+        rootDir: args.rootDir ?? path.resolve(),
+      })
+      break
+    }
+    default:
+      throw new Error(`Unknown repoType ${repoType}.`)
+  }
+}
+
+run(process.argv.slice(2)).catch(error => {
+  console.error(error)
+  process.exit(1)
+})
