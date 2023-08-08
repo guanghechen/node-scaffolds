621b3b2fb4f97cd1d1e84e8769ab6196b63fe5a0
guanghechen <me.guanghechen@gmail.com>
2023-08-08 22:40:36 +0800

    :wrench:  chore: fix jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,12 +5,10 @@ import url from 'node:url'
 
 export default async function () {
   const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+  const packageDir = path.relative(__dirname, path.resolve()) + '/'
   const baseConfig = await tsMonorepoConfig(__dirname, {
     useESM: true,
     tsconfigFilepath: path.join(__dirname, 'tsconfig.test.esm.json'),
-  })
-  const { default: manifest } = await import(path.resolve('package.json'), {
-    assert: { type: 'json' },
   })
 
   const config = {
@@ -24,15 +22,25 @@ export default async function () {
       'packages/helper-commander/src/util/yarn.ts',
       'packages/script-doc-link/src/cli.ts',
     ],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 90,
-        lines: 90,
-        statements: 90,
-        ...manifest.jest?.coverageThreshold?.global,
-      },
-    },
+    coverageThreshold: Object.fromEntries(
+      [
+        ['global', { branches: 80, functions: 90, lines: 90, statements: 90 }],
+        ['packages/helper-plop/src/cli.ts', { functions: 0, lines: 27, statements: 27 }],
+        [
+          'packages/helper-plop/src/run/types.ts',
+          { branches: 0, functions: 0, lines: 0, statements: 0 },
+        ],
+        [
+          'packages/helper-plop/src/run/util.ts',
+          { branches: 10, functions: 50, lines: 50, statements: 50 },
+        ],
+        ['packages/rollup-config/src/config.ts', { branches: 66 }],
+        ['packages/rollup-config/src/middleware/dts.ts', { branches: 60 }],
+        ['packages/rollup-config/src/middleware/ts.ts', { branches: 50 }],
+      ]
+        .filter(([p]) => !p.startsWith('packages/') || p.startsWith(packageDir))
+        .map(([p, val]) => (p.startsWith(packageDir) ? [path.join(__dirname, p), val] : [p, val])),
+    ),
     extensionsToTreatAsEsm: ['.ts', '.mts'],
     moduleNameMapper: {
       ...baseConfig.moduleNameMapper,
