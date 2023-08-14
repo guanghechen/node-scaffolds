80f88d5dc2755c58b92f60432b4376d5dbc501c7
guanghechen <me.guanghechen@gmail.com>
2023-08-14 23:03:14 +0800

    :art:  improve(rollup-config): re-organize files

--- packages/rollup-config/src/external.ts
+++ packages/rollup-config/src/external.ts
@@ -1,6 +1,48 @@
 import { collectAllDependencies, getDefaultDependencyFields } from '@guanghechen/helper-npm'
-import type { IEnv, IManifestWithDependencies } from '../types'
-import builtinModules from './builtin-modules.json' assert { type: 'json' }
+import type { IEnv, IManifestWithDependencies } from './types'
+
+const builtinModules = [
+  'assert',
+  'async_hooks',
+  'buffer',
+  'child_process',
+  'cluster',
+  'console',
+  'constants',
+  'crypto',
+  'dgram',
+  'dns',
+  'domain',
+  'events',
+  'fs',
+  'http',
+  'http2',
+  'https',
+  'inspector',
+  'module',
+  'net',
+  'os',
+  'path',
+  'perf_hooks',
+  'process',
+  'punycode',
+  'querystring',
+  'readline',
+  'repl',
+  'stream',
+  'string_decoder',
+  'timers',
+  'tls',
+  'trace_events',
+  'tty',
+  'url',
+  'util',
+  'v8',
+  'vm',
+  'wasi',
+  'worker_threads',
+  'zlib',
+]
 
 const builtinExternals: ReadonlyArray<string> = builtinModules.concat(['glob', 'sync'])
 export const builtinExternalSet: ReadonlySet<string> = new Set<string>(builtinExternals)
