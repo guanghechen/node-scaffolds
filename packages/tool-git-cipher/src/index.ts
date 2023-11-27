6aa280aa1451ba67227f2bc3e16abea62287768a
guanghechen <me.guanghechen@gmail.com>
2023-11-27 10:52:11 +0800

    update entry

--- packages/tool-git-cipher/src/index.ts
+++ packages/tool-git-cipher/src/index.ts
@@ -1,50 +1,64 @@
+// cat
 export * from './command/cat/command'
 export * from './command/cat/context'
 export * from './command/cat/option'
 export * from './command/cat/processor'
 export * from './command/cat/run'
+
+// decrypt
 export * from './command/decrypt/command'
 export * from './command/decrypt/context'
 export * from './command/decrypt/option'
 export * from './command/decrypt/processor'
 export * from './command/decrypt/run'
+
+// encrypt
 export * from './command/encrypt/command'
 export * from './command/encrypt/context'
 export * from './command/encrypt/option'
 export * from './command/encrypt/processor'
 export * from './command/encrypt/run'
+
+// init
 export * from './command/init/command'
 export * from './command/init/context'
 export * from './command/init/option'
 export * from './command/init/processor'
 export * from './command/init/run'
+
+// tree
 export * from './command/tree/command'
 export * from './command/tree/context'
 export * from './command/tree/option'
 export * from './command/tree/processor'
 export * from './command/tree/run'
+
+// verify
 export * from './command/verify/command'
 export * from './command/verify/context'
 export * from './command/verify/option'
 export * from './command/verify/processor'
 export * from './command/verify/run'
 
+// entry
+export * from './command/command'
+export * from './command/option'
+
+// shared
+export * from './shared/core/config'
+export * from './shared/core/constant'
 export * from './shared/core/error'
 export * from './shared/core/event'
 export * from './shared/core/reporter'
-export * from './command/command'
-export * from './command/option'
-export * from './shared/core/config'
-export * from './shared/core/constant'
 export * from './shared/util/context/createContext'
 export * from './shared/util/context/loadGitCipherContext'
-export * from './shared/CatalogCache'
-export * from './shared/util/input'
 export * from './shared/util/password/confirm'
 export * from './shared/util/password/input'
 export * from './shared/util/password/verify'
+export * from './shared/util/verifyCryptRepo'
+export * from './shared/util/verifyRepoStrictly'
+export * from './shared/util/input'
+export * from './shared/CatalogCache'
 export * from './shared/SecretConfig'
 export * from './shared/SecretConfig.types'
 export * from './shared/SecretMaster'
-export * from './shared/util/verifyCryptRepo'
-export * from './shared/util/verifyRepoStrictly'
